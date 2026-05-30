222f35bb9c55a6ec69c13914510ebbaddf87cdf6
shubhamgupta8899 <shubhamkrgupta2026@gmail.com>
2026-05-31 01:53:34 +0530

    feat: add RAG-based question answering with document retrieval

---
+++ src/main/java/com/shubham/RAGP01/controller/RagController.java
@@ -0,0 +1,80 @@
+package com.shubham.RAGP01.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+@RestController
+@RequestMapping("/api/rag")
+public class RagController {
+
+    private final ChatClient chatClient;
+    private final VectorStore vectorStore;
+
+    public RagController(ChatClient chatClient, VectorStore vectorStore){
+
+        this.chatClient = chatClient;
+        this.vectorStore = vectorStore;
+    }
+
+    @Value("classpath:/promptTemplates/ragTemplate.st")
+    Resource promptTemplate;
+
+    @GetMapping("/chat")
+    public ResponseEntity<String> chat(@RequestParam String question){
+
+        SearchRequest searchRequest = SearchRequest.builder()
+                .query(question)
+                .topK(4)
+                .similarityThreshold(0.4)
+                .build();
+
+        List<Document> similarDocs =  vectorStore.similaritySearch(searchRequest);
+        String content = similarDocs.stream()
+                .map(this::documentContent)
+                .filter(t -> t != null && !t.isBlank())
+                .collect(Collectors.joining(System.lineSeparator() + "----" + System.lineSeparator()));
+
+
+        if(content.isBlank()){
+
+            return ResponseEntity.ok("Sorry, I could not find relevent information");
+
+        }
+
+        String ans = chatClient.prompt()
+                .system(s-> s.text(promptTemplate)
+                        .param("documents", content))
+                .user(question)
+                .call()
+                .content();
+        return ResponseEntity.ok(ans);
+    }
+
+
+    // To check if document is null or not
+    private String documentContent(Document document){
+
+        String content = document.getFormattedContent();
+        if(content == null || content.isBlank()){
+
+            content = document.getText();
+        }
+
+        return content;
+
+    }
+
+}
